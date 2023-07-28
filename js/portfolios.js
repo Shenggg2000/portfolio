193c7ad9d20c215908e6ccbe1cc02026d9f4d1e1
Shenggg2000 <gohyoushengabc@gmail.com>
2023-07-28 22:26:04 +0800

    update skill section and portfolio

---
+++ js/portfolios.js
@@ -0,0 +1,213 @@
+var portfolio_data = [
+  {
+    id: "portfolio-smart-energy-monitoring",
+    images: [
+      {
+        src: '../images/portfolio-smart-energy-monitoring.png',
+        thumb: '../images/portfolio-smart-energy-monitoring.png',
+        subHtml: '<h4>Smart Energy Monitoring App</h4><p>Final Year Project</p>',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/1.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/1.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/2.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/2.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/3.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/3.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/4.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/4.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/5.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/5.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/6.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/6.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/7.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/7.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/8.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/8.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/9.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/9.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/10.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/10.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/11.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/11.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/12.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/12.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/13.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/13.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/14.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/14.png',
+        subHtml: '<h4>Image 1 title</h4><p>Image 1 descriptions.</p>',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/15.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/15.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/16.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/16.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/17.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/17.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/18.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/18.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/19.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/19.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/20.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/20.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/21.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/21.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/22.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/22.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/23.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/23.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/24.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/24.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/25.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/25.png',
+      },
+      {
+        src: '../images/portfolio-smart-energy-monitoring-images/26.png',
+        thumb: '../images/portfolio-smart-energy-monitoring-images/26.png',
+      },
+    ]
+  },
+  {
+    id: "portfolio-newpages-merchant",
+    images: [
+      {
+        src: '../images/portfolio-newpages-merchant.png',
+        thumb: '../images/portfolio-newpages-merchant.png',
+        subHtml: '<h4>Merchant Management System</h4><p>Newpages Network</p>',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/1.png',
+        thumb: '../images/portfolio-newpages-merchant-images/1.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/2.png',
+        thumb: '../images/portfolio-newpages-merchant-images/2.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/3.png',
+        thumb: '../images/portfolio-newpages-merchant-images/3.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/4.png',
+        thumb: '../images/portfolio-newpages-merchant-images/4.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/5.png',
+        thumb: '../images/portfolio-newpages-merchant-images/5.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/6.png',
+        thumb: '../images/portfolio-newpages-merchant-images/6.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/7.png',
+        thumb: '../images/portfolio-newpages-merchant-images/7.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/8.png',
+        thumb: '../images/portfolio-newpages-merchant-images/8.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/9.png',
+        thumb: '../images/portfolio-newpages-merchant-images/9.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/10.png',
+        thumb: '../images/portfolio-newpages-merchant-images/10.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/11.png',
+        thumb: '../images/portfolio-newpages-merchant-images/11.png',
+      },
+      {
+        src: '../images/portfolio-newpages-merchant-images/12.png',
+        thumb: '../images/portfolio-newpages-merchant-images/12.png',
+      },
+    ]
+  },
+  {
+    id: "portfolio-payroll",
+    images: [
+      {
+        src: '../images/portfolio-payroll.png',
+        thumb: '../images/portfolio-payroll.png',
+        subHtml: '<h4>Payroll System</h4><p>Coursework</p>',
+      },
+      {
+        src: '../images/portfolio-payroll-images/1.png',
+        thumb: '../images/portfolio-payroll-images/1.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/2.png',
+        thumb: '../images/portfolio-payroll-images/2.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/3.png',
+        thumb: '../images/portfolio-payroll-images/3.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/4.png',
+        thumb: '../images/portfolio-payroll-images/4.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/5.png',
+        thumb: '../images/portfolio-payroll-images/5.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/6.png',
+        thumb: '../images/portfolio-payroll-images/6.png',
+      },
+      {
+        src: '../images/portfolio-payroll-images/7.png',
+        thumb: '../images/portfolio-payroll-images/7.png',
+      },
+    ]
+  },
+]
